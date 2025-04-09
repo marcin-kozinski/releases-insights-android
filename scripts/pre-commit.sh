53ff1ffc0a0f27b1ef6c98423e65edf8ade5ebf0
Marcin Koziński <mkozinski@mozilla.com>
2025-04-09 13:15:30 +0200

    Set up ktfmt (#5)

---
+++ scripts/pre-commit.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Fail if any commands fails.
+set -e
+
+# Check dependencies
+if ! command -v ktfmt > /dev/null 2>&1; then
+    echo "Please install ktfmt with: brew install ktfmt"
+    exit 1
+fi
+
+UNSTAGED=$(git diff --name-only | sort)
+STAGED=$(git diff --cached --name-only | sort)
+FULLY_STAGED=$(comm -23 <(echo "$STAGED") <(echo "$UNSTAGED"))
+PARTIALLY_STAGED=$(comm -12 <(echo "$STAGED") <(echo "$UNSTAGED"))
+
+# Staged files that have no further modifications are safe and easy to reformat.
+if [[ -n "$FULLY_STAGED" ]]; then
+  echo "$FULLY_STAGED" | xargs scripts/reformat-code.sh
+  echo "$FULLY_STAGED" | xargs git add --
+fi
+
+# Partially staged files need extra precautions.
+if [[ -n "$PARTIALLY_STAGED" ]]; then
+  # Temporarily hide the unstaged changes.
+  git stash push --keep-index --message "before pre-commit code formatting check" --quiet
+  # Check formatting.
+  NOT_WELL_FORMATTED=$(echo "$STAGED" | xargs scripts/check-code-formatting.sh) \
+    || true # Don't exit this script if the check fails
+  # Restore unstaged modifications.
+  git reset --hard --quiet
+  git stash pop --index --quiet
+fi
+
+# If some partially staged files aren't well formatted..
+if [[ -n "$NOT_WELL_FORMATTED" ]]; then
+  # ..format them to make it easier for the developer..
+  echo "$NOT_WELL_FORMATTED" | xargs scripts/reformat-code.sh
+  # ..but don't try to stage them, leave that to the developer.
+  echo
+  echo "ERROR: Reformatted following files, but couldn't stage modifications safely:"
+  echo "$NOT_WELL_FORMATTED"
+  echo "Stage modifications manually and commit again."
+  exit 1
+fi
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,8 @@
+plugins { id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.22" }
+
+gitHooks {
+    preCommit { from { "scripts/pre-commit.sh" } }
+    createHooks(overwriteExisting = true)
+}
+
 rootProject.name = "releases-insights-android"
