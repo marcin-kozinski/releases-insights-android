3a96d6993c1ce4b6c870d134bee887d3b850fa70
Marcin Koziński <mkozinski@mozilla.com>
2025-04-10 12:20:59 +0200

    Set up version catalog format script

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -4,19 +4,23 @@
 set -e
 
 # Check dependencies
+if ! command -v kotlinc > /dev/null 2>&1; then
+    echo "Please install kotlin with: brew install kotlin"
+    exit 1
+fi
 if ! command -v ktfmt > /dev/null 2>&1; then
     echo "Please install ktfmt with: brew install ktfmt"
     exit 1
 fi
 
-UNSTAGED=$(git diff --name-only | sort)
-STAGED=$(git diff --cached --name-only | sort)
+UNSTAGED=$(git diff --diff-filter=ACMRT --name-only | sort)
+STAGED=$(git diff --cached --diff-filter=ACMRT --name-only  | sort)
 FULLY_STAGED=$(comm -23 <(echo "$STAGED") <(echo "$UNSTAGED"))
 PARTIALLY_STAGED=$(comm -12 <(echo "$STAGED") <(echo "$UNSTAGED"))
 
 # Staged files that have no further modifications are safe and easy to reformat.
 if [[ -n "$FULLY_STAGED" ]]; then
-  echo "$FULLY_STAGED" | xargs scripts/reformat-code.sh
+  echo "$FULLY_STAGED" | xargs scripts/check-code-formatting.sh --fix
   echo "$FULLY_STAGED" | xargs git add --
 fi
 
@@ -35,7 +39,7 @@ fi
 # If some partially staged files aren't well formatted..
 if [[ -n "$NOT_WELL_FORMATTED" ]]; then
   # ..format them to make it easier for the developer..
-  echo "$NOT_WELL_FORMATTED" | xargs scripts/reformat-code.sh
+  echo "$NOT_WELL_FORMATTED" | xargs scripts/check-code-formatting.sh --fix
   # ..but don't try to stage them, leave that to the developer.
   echo
   echo "ERROR: Reformatted following files, but couldn't stage modifications safely:"
