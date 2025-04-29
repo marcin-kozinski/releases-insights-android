cd3b6bba5c9c714d78aa9b63114a8b73cd9b8141
Marcin Koziński <mkozinski@mozilla.com>
2025-04-29 09:50:30 +0200

    Drop duplicate plugin version definition (#16)

--- build-logic/settings/src/main/kotlin/releasesinsights/pre-commit-git-hooks.settings.gradle.kts
+++ build-logic/settings/src/main/kotlin/releasesinsights/pre-commit-git-hooks.settings.gradle.kts
@@ -1,6 +1,6 @@
 package releasesinsights
 
-plugins { id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.22" }
+plugins { id("org.danilopianini.gradle-pre-commit-git-hooks") }
 
 gitHooks {
     preCommit { from { "scripts/pre-commit.sh" } }
