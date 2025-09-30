68d95851558045ec77948537f98da62c4e735845
Marcin Koziński <mkozinski@mozilla.com>
2025-09-30 12:23:25 +0200

    Enable dependency analysis plugin (#47)

--- source/releases-insights-api/build.gradle.kts
+++ source/releases-insights-api/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    alias(libs.plugins.dependency.analysis)
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.metro)
@@ -10,13 +11,20 @@ dependencies {
     platform(libs.kotlinx.coroutines.bom)
 
     api(libs.kotlinx.datetime)
+    api(libs.kotlinx.serialization.core)
     api(ktorLibs.client.core)
     implementation(libs.kotlinx.serialization.json)
     implementation(ktorLibs.client.contentNegotiation)
     implementation(ktorLibs.client.resources)
+    implementation(ktorLibs.http)
+    implementation(ktorLibs.resources)
+    implementation(ktorLibs.serialization)
     implementation(ktorLibs.serialization.kotlinx.json)
+    implementation(ktorLibs.utils)
 
     testImplementation(libs.kotlin.test)
     testImplementation(libs.kotlinx.coroutines.test)
     testImplementation(ktorLibs.client.mock)
+    testImplementation(ktorLibs.http)
+    testImplementation(ktorLibs.utils)
 }
